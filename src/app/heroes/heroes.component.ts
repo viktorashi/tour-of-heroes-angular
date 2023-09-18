2bb59510cb6a06971155d68f8ca9e4374fdb7bff
Viktorashi <stancastanvictorioan@gmail.com>
2023-09-18 15:09:23 +0300

    bruh

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,16 +1,12 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: [ './heroes.component.css' ]
 })
 export class HeroesComponent {
-  hero : Hero= {
-    name: 'Gigel' ,
-    id:1,
-    birthday : new Date(2004, 1, 26),
-    price : 6.9
-  };
+  heroes = HEROES
   _defaultCurrencyCode = "EUR"
 }
