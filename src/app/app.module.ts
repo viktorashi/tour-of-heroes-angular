4c3656a536d9e43eeabc1c360272b39a93ccecd9
viktorashi <33178074+viktorashi@users.noreply.github.com>
2023-09-23 14:39:58 +0300

    Facui ca si cum cu reactive js si sunt pe cale sa pun mesaje dupa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
+import { HeroDetailsComponent } from './hero-details/hero-details.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailsComponent
   ],
   imports: [
     BrowserModule,
